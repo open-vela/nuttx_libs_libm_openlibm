806abdd830518446a78e0cf729ee475934d775ac
liujinye <liujinye@xiaomi.com>
2020-08-26 14:58:02 +0800

    Merge pull request #208 from cndesantana/patch-1
    Fix small typo

--- include/openlibm_math.h
+++ include/openlibm_math.h
@@ -18,8 +18,6 @@
 #include <math.h>
 #else /* !OPENLIBM_USE_HOST_MATH_H */
 
-#include <openlibm_defs.h>
-
 #ifndef OPENLIBM_MATH_H
 #define	OPENLIBM_MATH_H
 
@@ -33,6 +31,16 @@
 
 #ifndef __pure2
 #define __pure2
+#endif
+
+#ifdef _WIN32
+# ifdef IMPORT_EXPORTS
+#  define OLM_DLLEXPORT __declspec(dllimport)
+# else
+#  define OLM_DLLEXPORT __declspec(dllexport)
+# endif
+#else
+#define OLM_DLLEXPORT __attribute__ ((visibility("default")))
 #endif
 
 /*
